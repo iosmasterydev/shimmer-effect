53b629778fe791a4eba9c07cf1ea9ec7108a3f8a
Ibrahima Ciss <bionik6@gmail.com>
2023-01-23 01:07:25 +0000

    Initial Commit

---
+++ Shimmer/ShimmerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShimmerApp.swift
+//  Shimmer
+//
+//  Created by Ibrahima Ciss on 23/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct ShimmerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
